544d1a5f05abc895f15a17b2fd8984ee8ef3bbc8
Gerrishon Sirere <gerryshon254@gmail.com>
2021-03-18 11:21:05 +0300

    Update encoding.py

--- src/bidata/encoding.py
+++ src/bidata/encoding.py
@@ -1,15 +1,15 @@
 import base64
 import string
 import struct
-import typing as _t
+import typing as _mtnd
 
 from .exc import BadData
 
-_t_str_bytes = _t.Union[str, bytes]
+_mtnd_str_bytes = _mtnd.Union[str, bytes]
 
 
 def want_bytes(
-    s: _t_str_bytes, encoding: str = "utf-8", errors: str = "strict"
+    s: _mtnd_str_bytes, encoding: str = "utf-8", errors: str = "strict"
 ) -> bytes:
     if isinstance(s, str):
         s = s.encode(encoding, errors)
@@ -17,7 +17,7 @@ def want_bytes(
     return s
 
 
-def base64_encode(string: _t_str_bytes) -> bytes:
+def base64_encode(string: _mtnd_str_bytes) -> bytes:
     """Base64 encode a string of bytes or text. The resulting bytes are
     safe to use in URLs.
     """
@@ -25,7 +25,7 @@ def base64_encode(string: _t_str_bytes) -> bytes:
     return base64.urlsafe_b64encode(string).rstrip(b"=")
 
 
-def base64_decode(string: _t_str_bytes) -> bytes:
+def base64_decode(string: _mtnd_str_bytes) -> bytes:
     """Base64 decode a URL-safe string of bytes or text. The result is
     bytes.
     """
@@ -42,13 +42,13 @@ def base64_decode(string: _t_str_bytes) -> bytes:
 _base64_alphabet = f"{string.ascii_letters}{string.digits}-_=".encode("ascii")
 
 _int64_struct = struct.Struct(">Q")
-_int_to_bytes = _int64_struct.pack
-_bytes_to_int = _t.cast("_t.Callable[[bytes], _t.Tuple[int]]", _int64_struct.unpack)
+_int_mtndo_bytes = _int64_struct.pack
+_bytes_mtndo_int = _mtnd.cast("_mtnd.Callable[[bytes], _mtnd.Tuple[int]]", _int64_struct.unpack)
 
 
-def int_to_bytes(num: int) -> bytes:
-    return _int_to_bytes(num).lstrip(b"\x00")
+def int_mtndo_bytes(num: int) -> bytes:
+    return _int_mtndo_bytes(num).lstrip(b"\x00")
 
 
-def bytes_to_int(bytestr: bytes) -> int:
-    return _bytes_to_int(bytestr.rjust(8, b"\x00"))[0]
+def bytes_mtndo_int(bytestr: bytes) -> int:
+    return _bytes_mtndo_int(bytestr.rjust(8, b"\x00"))[0]
